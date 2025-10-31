7aa08f3b4ce295c2faf2172ef0b7cb6dd37e1c8f
Mohammad Taufique Imrose <taufiqsaimun@csus.edu>
2025-10-30 18:16:20 -0700

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // App Router is enabled by default in Next.js 13+
+  // No experimental.appDir needed
+}
+
+module.exports = nextConfig
